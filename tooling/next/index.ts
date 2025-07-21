0d34504222eb5a5ea4b8f12ce3f6651f3197fa7b
MasahiroSakoda <thunderbird.m.s@gmail.com>
2025-07-21 17:49:07 +0900

    feat: activate next.js shared config

--- tooling/next/index.ts
+++ tooling/next/index.ts
@@ -16,9 +16,18 @@ export const config: NextConfig = {
 
 	async rewrites() {
 		return [
-			{ source: "/ingest/static/:path*", destination: "https://us-assets.i.posthog.com/static/:path*" },
-			{ source: "/ingest/:path*",        destination: "https://us.i.posthog.com/:path*" },
-			{ source: "/ingest/decide",        destination: "https://us.i.posthog.com/decide" },
+			{
+				source: "/ingest/static/:path*",
+				destination: "https://us-assets.i.posthog.com/static/:path*",
+			},
+			{
+				source: "/ingest/:path*",
+				destination: "https://us.i.posthog.com/:path*",
+			},
+			{
+				source: "/ingest/decide",
+				destination: "https://us.i.posthog.com/decide",
+			},
 		];
 	},
 
